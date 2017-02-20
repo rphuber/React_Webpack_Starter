88b41a99084f1045f5b34f51c3a84768ebbe89b4
rphuber <ryan.huber@mac.com>
2017-02-20 09:39:52 -0500

    initial setup

--- README.md
+++ README.md
@@ -2,14 +2,18 @@ React with Webpack Starter Files
 ----
 
 What this includes:
+- Webpack (v.2)
 - React
-- Webpack
+- Redux (w/react-redux)
+- Sass
 
 How to use:
 - clone this repo ```git clone git@github.com:rphuber/React_Webpack_Starter.git```
 - change directory name to your project name ```mv React_Webpack_Starter <your_project_name>```
 - change directory into your project ```cd <your_project_name>```
 - install npm dependencies ```npm install```
+- remove git repository information ```rm -rf .git```
+- initiate as a new git repository ```git init```
 - see scripts for options from this point
 
 Things to add:
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const resolve = require('path').resolve;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+	return {
+		entry: './source/index.js',
+		output: {
+			path: resolve(__dirname, './build'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: resolve(__dirname, './source'),
+					loader: 'babel-loader',
+					options: {
+						presets: ['react', ['es2015', { modules: false }]]
+					}
+				},
+				{
+					test: /\.scss$/,
+					include: resolve(__dirname, './source'),
+					use: [
+						'style-loader',
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								localIdentName: '[name]__[local]--[hasj:base64:5]'
+							}
+						},
+						'sass-loader'
+					]
+				},
+				{
+					test: /\.html$/,
+					include: resolve(__dirname, './source'),
+					loader: 'html-loader'
+				}
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: 'source/index.html'
+			})
+		],
+		devServer: {
+			contentBase: resolve(__dirname, './build'),
+			port: 8081
+		}
+	}
+}
