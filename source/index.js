2e82ea22395d06963ebcbdc5b39cbeca5301ff4d
rphuber <ryan.huber@mac.com>
2017-02-20 10:26:07 -0500

    move app to its own directory

--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components';
+import App from './components/App';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
